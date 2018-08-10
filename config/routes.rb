aaab5759e998373fbd9e1b6779a2b613c6002ce0
stoneLeaf <owi.stoneleaf@gmail.com>
2018-08-10 09:17:33 +0200

    Pages controller and devise basic setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'general#home'
+
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/tos'
+  get 'pages/contact'
+  
 end
