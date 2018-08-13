485112e0bfd5ab867e7ba798706ca8cd52e9ebf6
stoneLeaf <owi.stoneleaf@gmail.com>
2018-08-13 10:53:17 +0200

    User model generated and configured for Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  root 'general#home'
+  devise_for :users
+  root 'pages#home'
 
   get 'pages/home'
   get 'pages/about'
   get 'pages/tos'
   get 'pages/contact'
-  
+
 end
