6291f7f8e015f2db7cadc7d4f309667a5508f30c
stoneLeaf <owi.stoneleaf@gmail.com>
2018-08-14 15:19:28 +0200

    Added mail configuration for prod

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'noreply@evening-reaches-85072.herokuapp.com'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Needed by Devise
+  config.action_mailer.default_url_options = { :host => 'evening-reaches-85072.herokuapp.com' }
 end
