4900cc629e2039f5bd013c4ef7c18343567d9805
stoneLeaf <owi.stoneleaf@gmail.com>
2018-11-08 23:37:53 +0100

    end_date validation for Record model

--- app/models/record.rb
+++ app/models/record.rb
@@ -8,6 +8,8 @@ class Record < ApplicationRecord
   validate :end_after_start
 
   def end_after_start
-    # 
+    if end_date.present? && end_date <= start_date
+      errors.add(:end_date, "must be after start_date")
+    end
   end
 end
