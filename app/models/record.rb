2fbd16acc77b100f1d57a6cd6603a836dfa8278d
stoneLeaf <owi.stoneleaf@gmail.com>
2018-11-08 13:48:01 +0100

    Created fixtures

--- app/models/record.rb
+++ app/models/record.rb
@@ -5,4 +5,9 @@ class Record < ApplicationRecord
   validates :user_id, presence: true
   validates :project_id, presence: true
   validates :start, presence: true
+  validate :end_after_start
+
+  def end_after_start
+    # 
+  end
 end
