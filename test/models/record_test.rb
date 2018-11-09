a1b64587b7352e076ae9c6e58e882dc3b4d961b0
stoneLeaf <owi.stoneleaf@gmail.com>
2018-11-09 10:07:58 +0100

    Updated Record model tests

--- test/models/record_test.rb
+++ test/models/record_test.rb
@@ -8,14 +8,14 @@ class RecordTest < ActiveSupport::TestCase
     @record = Record.new(user: @user, project: @project, start_date: Time.now)
   end
 
-  test "should be valid" do
-    assert @record.valid?
+  test "basic presence validation" do
+    assert_not @record.update_attributes(user: nil)
+    assert_not @record.update_attributes(project: nil)
+    assert_not @record.update_attributes(start_date: nil)
   end
 
   test "end_date should be after start_date" do
-    @record.update_attribute(:end_date, @record.start_date + 1.hour)
-    assert @record.valid?
-    @record.update_attribute(:end_date, @record.start_date - 1.hour)
-    assert_not @record.valid?
+    assert @record.update_attributes(end_date: @record.start_date + 1.hour)
+    assert_not @record.update_attributes(end_date: @record.start_date - 1.hour)
   end
 end
